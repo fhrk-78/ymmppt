e08044a762f200d7ffa0e8c82962af476917d604
PizzaHarumaki <78fhruby78@gmail.com>
2024-06-28 09:03:05 +0900

    init repo

---
+++ ymm-project-packingtool/ConvertDirectory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ymm_project_packingtool
+{
+    internal class ConvertDirectory
+    {
+    }
+}
---
+++ ymm-project-packingtool/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ymm_project_packingtool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
